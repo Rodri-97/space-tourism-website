77fdd63ea2f93d2d8aa4a45efeab6a4a6f485748
rodrigo <rodrigo_coelho@orange.fr>
2022-09-03 16:53:58 +0200

    set up routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './routes/Home';
+import Destination from './routes/Destination';
+import Crew from './routes/Crew';
+import Technology from './routes/Technology';
+
 const App = () => (
-  <div>
-    <h1>Space Tourism Website</h1>
-  </div>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='destination' element={<Destination />} />
+      <Route path='crew' element={<Crew />} />
+      <Route path='technology' element={<Technology />}></Route>
+    </Routes>
+    <nav>
+      <Link to='/'>Home</Link>
+      <Link to='/destination'>Destination</Link>
+      <Link to='/crew'>Crew</Link>
+      <Link to='/technology'>Technology</Link>
+    </nav>
+  </BrowserRouter>
 );
 
 export default App;
