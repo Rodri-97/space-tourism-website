33adb4958023726a4e6f1033f7b0affbd012c673
rodrigo <rodrigo_coelho@orange.fr>
2022-09-10 15:48:52 +0200

    crew page logic and mobile design done

--- src/routes/Crew/Crew.js
+++ src/routes/Crew/Crew.js
@@ -1,7 +1,47 @@
-import { StyledCrew } from './Crew.styles';
+import { StyledCrew, InfoContainer } from './Crew.styles';
+import { useState } from 'react';
+import data from '../../data.json';
 
 const Crew = () => {
-  return <StyledCrew>Crew</StyledCrew>;
+  const [selectedPerson, setSelectedPerson] = useState(data.crew[0]);
+
+  const allBullets = data.crew.map((person, idx) => {
+    return (
+      <li
+        className='bullet'
+        onClick={() => setSelectedPerson(data.crew[idx])}
+        key={person.name}
+        style={
+          selectedPerson.name === person.name
+            ? { backgroundColor: 'white' }
+            : null
+        }
+      ></li>
+    );
+  });
+
+  return (
+    <StyledCrew>
+      <h4 className='page-title'>
+        <span>02</span> MEET YOUR CREW
+      </h4>
+
+      <img
+        src={selectedPerson.images.png}
+        alt={selectedPerson.name}
+        className='person-img'
+      />
+
+      <InfoContainer className='info-container'>
+        <ul className='bullets'>{allBullets}</ul>
+        <div className='main-info'>
+          <h3 className='role'>{selectedPerson.role.toUpperCase()}</h3>
+          <h2 className='name'>{selectedPerson.name.toUpperCase()}</h2>
+        </div>
+        <p>{selectedPerson.bio}</p>
+      </InfoContainer>
+    </StyledCrew>
+  );
 };
 
 export default Crew;
--- src/routes/Crew/Crew.styles.js
+++ src/routes/Crew/Crew.styles.js
@@ -1,5 +1,60 @@
 import styled from 'styled-components';
+import { minimalTabletWidth } from '../../helpers/styleData';
+
+const infoGap = 30;
 
 export const StyledCrew = styled.main`
-  background-color: orange;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .person-img {
+    width: 250px;
+    height: 300px;
+    margin-top: 30px;
+  }
+`;
+
+export const InfoContainer = styled.article`
+  .bullets {
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+    list-style: none;
+
+    .bullet {
+      background-color: rgba(255, 255, 255, 0.3);
+      border-radius: 50%;
+      width: 15px;
+      height: 15px;
+      cursor: pointer;
+
+      &:hover {
+        background-color: rgba(255, 255, 255, 0.6);
+      }
+    }
+  }
+
+  .main-info {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    text-align: center;
+
+    .role {
+      color: rgba(255, 255, 255, 0.5);
+    }
+
+    .name {
+      font-size: 25px;
+    }
+  }
+
+  @media screen and (max-width: ${minimalTabletWidth}px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: ${infoGap}px;
+    padding: ${infoGap}px 30px;
+  }
 `;
