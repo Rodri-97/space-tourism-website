024974fde2d23bcf42aa65e998ae1614aa665894
rodrigo <rodrigo_coelho@orange.fr>
2022-09-10 18:16:23 +0200

    crew design done

--- src/routes/Crew/Crew.styles.js
+++ src/routes/Crew/Crew.styles.js
@@ -1,31 +1,28 @@
 import styled from 'styled-components';
-import { minimalTabletWidth } from '../../helpers/styleData';
+import {
+  minimalDesktopWidth,
+  minimalTabletWidth,
+} from '../../helpers/styleData';
 
 const infoGap = 30;
 
 export const StyledCrew = styled.main`
-  .person-img {
-    width: 250px;
-    height: 300px;
-    margin-top: 30px;
-  }
-
   @media screen and (max-width: ${minimalTabletWidth}px) {
     display: flex;
     flex-direction: column;
     align-items: center;
+
+    .person-img {
+      width: 250px;
+      height: 300px;
+      margin-top: 30px;
+    }
   }
 
   @media screen and (min-width: ${minimalTabletWidth}px) {
     display: grid;
     justify-items: center;
-    grid-template-areas: 'title' 'info' 'image';
     margin-top: 50px;
-
-    .page-title {
-      grid-area: title;
-      margin-bottom: 30px;
-    }
 
     .person-img {
       grid-area: image;
@@ -35,6 +32,30 @@ export const StyledCrew = styled.main`
 
     .info-container {
       grid-area: info;
+    }
+  }
+
+  @media screen and (min-width: ${minimalTabletWidth}px) and (max-width: ${minimalDesktopWidth}px) {
+    grid-template-areas: 'title' 'info' 'image';
+
+    .page-title {
+      grid-area: title;
+      margin-bottom: 30px;
+    }
+
+    .person-img {
+      margin-top: 50px;
+    }
+  }
+
+  @media screen and (min-width: ${minimalDesktopWidth}px) {
+    grid-template-areas: 'info image';
+    grid-template-columns: repeat(2, 1fr);
+
+    .person-img {
+      margin-top: 150px;
+      height: 500px;
+      width: 400px;
     }
   }
 `;
@@ -63,14 +84,13 @@ export const InfoContainer = styled.article`
     display: flex;
     flex-direction: column;
     gap: 10px;
-    text-align: center;
 
     .role {
       color: rgba(255, 255, 255, 0.5);
     }
 
     .name {
-      font-size: 25px;
+      color: white;
     }
   }
 
@@ -80,11 +100,16 @@ export const InfoContainer = styled.article`
     align-items: center;
     gap: ${infoGap}px;
     padding: ${infoGap}px 30px;
+
+    .main-info {
+      .name {
+        font-size: 25px;
+      }
+    }
   }
 
   @media screen and (min-width: ${minimalTabletWidth}px) {
     display: grid;
-    justify-items: center;
     grid-template-areas: 'info' 'bio' 'bullets';
     gap: 10px;
     margin-top: 50px;
@@ -96,11 +121,49 @@ export const InfoContainer = styled.article`
     .bio {
       grid-area: bio;
       font-size: 20px;
-      padding: 0 100px;
     }
 
     .bullets {
       grid-area: bullets;
+    }
+  }
+
+  @media screen and (min-width: ${minimalTabletWidth}px) and (max-width: ${minimalDesktopWidth}px) {
+    justify-items: center;
+
+    .bio {
+      padding: 0 100px;
+    }
+  }
+
+  @media screen and (max-width: ${minimalDesktopWidth}px) {
+    .main-info {
+      text-align: center;
+    }
+  }
+
+  @media screen and (min-width: ${minimalDesktopWidth}px) {
+    justify-items: left;
+    grid-template-rows: repeat(3, 1fr);
+
+    .bullets {
+      align-items: start;
+    }
+
+    .main-info {
+      justify-content: end;
+
+      .role {
+        font-size: 25px;
+      }
+
+      .name {
+        font-size: 40px;
+      }
+    }
+
+    .bio {
+      padding-right: 50px;
     }
   }
 `;
