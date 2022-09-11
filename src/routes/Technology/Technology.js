83bb13ea15f402bbd8ff97216c958a44cd588fcc
rodrigo <rodrigo_coelho@orange.fr>
2022-09-11 15:31:20 +0200

    technology page logic and markup done

--- src/routes/Technology/Technology.js
+++ src/routes/Technology/Technology.js
@@ -1,7 +1,44 @@
-import { StyledTechnology } from './Technology.styles';
+import { StyledTechnology, StyledContent } from './Technology.styles';
+import data from '../../data.json';
+import { useState } from 'react';
 
 const Technology = () => {
-  return <StyledTechnology>Technology</StyledTechnology>;
+  const [currentTechnology, setCurrentTechnology] = useState(
+    data.technology[0]
+  );
+
+  const sliders = data.technology.map((technology, idx) => {
+    return (
+      <li
+        key={technology.name}
+        onClick={() => setCurrentTechnology(technology)}
+      >
+        {idx + 1}
+      </li>
+    );
+  });
+
+  return (
+    <StyledTechnology>
+      <h4 className='page-title'>
+        <span>02</span> MEET YOUR CREW
+      </h4>
+
+      <StyledContent>
+        <img
+          src={currentTechnology.images.landscape}
+          alt={currentTechnology.name}
+        />
+        <ul>{sliders}</ul>
+
+        <section className='text-content'>
+          <h3>THE TERMINOLOGY</h3>
+          <h2>{currentTechnology.name}</h2>
+          <p>{currentTechnology.description}</p>
+        </section>
+      </StyledContent>
+    </StyledTechnology>
+  );
 };
 
 export default Technology;
--- src/routes/Technology/Technology.styles.js
+++ src/routes/Technology/Technology.styles.js
@@ -1,5 +1,13 @@
 import styled from 'styled-components';
 
 export const StyledTechnology = styled.main`
-  background-color: green;
+  border: 3px solid red;
+
+  > * {
+    border: 3px solid orange;
+  }
+`;
+
+export const StyledContent = styled.article`
+  color: orange;
 `;
